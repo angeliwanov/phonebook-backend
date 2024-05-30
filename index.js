365e291dae46a2d1a4dc0fa370614fc7b9ed1f31
Angel Ivanov <angel.iwanov@gmail.com>
2024-05-30 18:04:41 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
+const Person = require("./models/person");
 
 //MIDDLEWARE JSON
 app.use(express.json());
@@ -17,32 +19,9 @@ morgan.token("postData", (request) => {
 //MIDDLEWARE ALL REQUESTS CONFIG
 app.use(morgan(":method :url :response-time ms :postData"));
 
-//LOCAL DB
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 //GET ALL PERSONS
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", async (request, response, next) => {
+  const persons = await Person.find({});
   response.json(persons);
 });
 
@@ -57,28 +36,51 @@ app.get("/info", (request, response) => {
 });
 
 //GET PERSON
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
+app.get("/api/persons/:id", async (request, response, next) => {
+  try {
+    const person = await Person.findById(request.params.id);
+    if (person) {
+      response.json(person);
+    } else {
+      request.status(404).end();
+    }
+  } catch (error) {
+    next(error);
   }
 });
 
 //DELETE PERSON
-app.delete("/api/persons/:id", (request, respons) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((p) => p.id !== id);
+app.delete("/api/persons/:id", async (request, response, next) => {
+  try {
+    await Person.findByIdAndDelete(request.params.id);
+    response.sendStatus(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
 
-  respons.sendStatus(204).end();
+app.put("/api/persons/:id", async (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  try {
+    const updatedPerson = await Person.findByIdAndUpdate(
+      request.params.id,
+      person,
+      { new: true }
+    );
+    response.json(updatedPerson);
+  } catch (error) {
+    next(error);
+  }
 });
 
 //POST - CREATE PERSON
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", async (request, response) => {
   const body = request.body;
 
   if (!body.name) {
@@ -87,25 +89,37 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (persons.find((p) => p.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  const person = {
-    id: Math.floor(Math.random() * 10000000),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  const savedPerson = await person.save();
+  response.json(savedPerson);
 });
 
+//unknownEndpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+//Error handling
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatter id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
 //SERVER
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as agrument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://angeliwanov:${password}@cluster0.w5bdlyv.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+console.log("connected to db");
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+});
+
+if (process.argv.length === 5) {
+  person.save().then((result) => {
+    console.log("phonebook saved");
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((persons) => {
+    persons.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
